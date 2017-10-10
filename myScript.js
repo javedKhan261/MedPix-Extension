39892df39829aa4a850077ebc0051a5481e4b484
javedkhanCurofy <javed.khan@curofy.com>
2017-10-10 13:53:23 +0530

    trying to make buttons aside cases once loader disappers

--- myScript.js
+++ myScript.js
@@ -1,1 +1,25 @@
-alert(document.domain);
+if (location.host=="app.figure1.com") {
+	debugger;
+	window.onload = function() {
+		var div = document.createElement("div");
+	  div.style.position = "relative";
+	  div.style.padding = "5px";
+	  div.style.backgroundColor = "whitesmoke";
+	  div.style.margin = "5px";
+	  div.innerHTML = "hello";
+	  $('html').append(div);
+	};
+} else {
+	image_urls = [];
+
+	image_elements_on_page = $(".slider-image")
+
+	for(var i=0;i<image_elements_on_page;i++) {
+		image_thumbnail_url = image_elements_on_page[0].src;
+		image_full_url = image_thumbnail_url.replace('thumb','full');
+		image_urls.push(image_full_url);
+	}
+}
+
+
+
