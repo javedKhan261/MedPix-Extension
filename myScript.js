9017be64d08bb83a68b4b2627b7990120db4da0a
javedkhanCurofy <javed.khan@curofy.com>
2017-10-24 15:45:43 +0530

    created button for adding case

--- myScript.js
+++ myScript.js
@@ -1,14 +1,28 @@
+chrome.runtime.onMessage.addListener(function(request) {
+    if(request.action === 'executeCode') {
+    		var div = document.createElement("div");
+        div.className = "add-to-curofy-button";
+        div.setAttribute('align','center');
+        div.setAttribute("style", "border-style: solid;border-color: #08794a;border-radius: 10px;font-weight: bolder;");
+        div.style.position = "relative";
+        div.style.padding = "5px";
+        div.style.backgroundColor = "#169B86";
+        div.style.color = "#FFFFFF";
+        div.style.top = "20px";
+        div.style.height = "30px";
+        div.style.cursor = "pointer";
+        div.style.opacity = "0.8";
+        div.style.margin = "5px";
+        div.innerHTML = "Add Case to Curofy";
+        $('.add-to-curofy-button').remove();
+        $('li.feed-card').append(div);
+    		console.log("hello world");
+    }
+});
+
+
 if (location.host=="app.figure1.com") {
 	debugger;
-	window.onload = function() {
-		var div = document.createElement("div");
-	  div.style.position = "relative";
-	  div.style.padding = "5px";
-	  div.style.backgroundColor = "whitesmoke";
-	  div.style.margin = "5px";
-	  div.innerHTML = "hello";
-	  $('html').append(div);
-	};
 } else {
 	image_urls = [];
 
