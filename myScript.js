e76c140345f9d202d9393ea2e8ff4b094df27607
javedkhanCurofy <javed.khan@curofy.com>
2017-10-31 17:19:54 +0530

    data to send ready

--- myScript.js
+++ myScript.js
@@ -11,15 +11,36 @@ chrome.runtime.onMessage.addListener(function(request) {
         div.style.top = "20px";
         div.style.height = "30px";
         div.style.cursor = "pointer";
-        div.style.opacity = "0.8";
         div.style.margin = "5px";
         div.innerHTML = "Add Case to Curofy";
         $('.add-to-curofy-button').remove();
         $('li.feed-card').append(div);
-    		console.log("hello world");
+        $('.add-to-curofy-button').click(function() {
+        		debugger;
+        		main_case_div = $(this).parent();
+        		main_case_description = main_case_div.find('.main-case-info').find('.caption').text();
+        		image_section = main_case_div.find('.image-section');
+        		main_image_src = image_section.find('.detailed-image-wrapper').find('img').attr('src');
+        		main_image_src_spilt = main_image_src.split('&');
+        		suffix_for_full_size_image = main_image_src_spilt[main_image_src_spilt.length-1];
+        		image_set_thumbmail_elements = image_section.find('.image-set-thumbnails').find('li').find('img');
+        		image_src_array = [];
+        		for(i=0;i<image_set_thumbmail_elements.length;i++) {
+        			thumbnail_src = image_set_thumbmail_elements[i].src;
+        			thumbnail_src_split = thumbnail_src.split('&');
+        			full_image_src = thumbnail_src_split[0]+'&'+suffix_for_full_size_image;
+        			image_src_array.push(full_image_src);
+        		}
+
+        		var data_to_send = {};
+        		data_to_send['description'] = main_case_description;
+        		data_to_send['images'] = image_src_array;
+        		debugger;
+        		
+        		
+        });
     }
 });
-
 
 if (location.host=="app.figure1.com") {
 	debugger;
